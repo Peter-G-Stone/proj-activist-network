f881c4c6571ce5b9cc1fb110ac38e77473e28fef
Peter-G-Stone <peter.stone.online@gmail.com>
2018-12-19 17:13:38 -0500

    added views files for gropus and users, started writing group views. wrote the index view using a partial _group. now writing the edit view so I can joyfully edit some of the seed data that I no longer find amusing.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,49 @@
 class GroupsController < ApplicationController
-end
+
+    before_action :set_group, only: [:show, :edit, :update]
+    before_action :authenticate_user!
+
+
+
+    def index
+    #   @attractions = Attraction.all
+        @groups = Group.all
+    end
+  
+    def show
+    end
+  
+    def new
+    #   @attraction = Attraction.new
+    end
+  
+    def create
+    #   attraction = Attraction.create(attraction_params)
+    #   redirect_to attraction_path(attraction)
+    end
+  
+    def edit
+      
+    end
+  
+    def update
+  
+    #   @attraction.update(attraction_params)
+    #   redirect_to attraction_path(@attraction)
+    end
+  
+    private
+      def attraction_params
+        # params.require(:attraction).permit(:name, :nausea_rating, :happiness, :tickets, :min_height)
+      end
+  
+      def set_group
+        @group = Group.find(params[:id])
+      end
+  
+      def set_user
+        # @user = current_user
+      end
+  
+  end
+  
