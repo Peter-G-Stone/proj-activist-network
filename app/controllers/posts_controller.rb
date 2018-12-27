970c0e3da6b2e81036c3ee2d8020f19b2fb02937
Peter-G-Stone <peter.stone.online@gmail.com>
2018-12-27 18:13:50 -0500

    working to add post creation nested within the group page, seems good now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,19 @@
 class PostsController < ApplicationController
 
     def new
-        post = Post.new
+        @post = Post.new
+        @post.user = current_user
+        @post.group = Group.find(params[:group_id])
     end
 
     def create
+        @post = Post.create(content: post_params[:content], group: Group.find(post_params[:group_id]), user: current_user)
+        redirect_to group_path(@post.group)
+    end
 
+    private
+
+    def post_params
+        params.require(:post).permit(:content, :group_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,21 @@ Rails.application.routes.draw do
 
 
   #custom routes:
+  #
+  #
   get '/groups/:id/join_group', to: 'groups#join_group'
   get '/groups/:id/leave_group', to: 'groups#leave_group'
   
   
   #standard routes:
-  # resources :posts
+  #
+  #
+  resources :posts, only: [:create, :update]
   resources :groups do 
     resources :posts, except: [:index, :show]
   end
   devise_for :users, :controllers => { registrations: 'registrations' }
+
 
   root 'groups#index'
 
