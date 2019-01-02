f2d1f2df0e1a794eb79f4dc088be3344f8c05f0c
Peter-G-Stone <peter.stone.online@gmail.com>
2019-01-02 14:36:16 -0500

    created comments, theyre displayed on group show pages under their post, also now only the author of a post can edit or delete it, also now only a member of a group can post

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 
   has_and_belongs_to_many :groups
   has_many :posts
+  has_many :comments
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
