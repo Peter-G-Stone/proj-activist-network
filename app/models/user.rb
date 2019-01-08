3dde9d5f8ef069a7334d658135835650bfc2cf82
Peter-G-Stone <peter.stone.online@gmail.com>
2019-01-07 19:05:22 -0500

    working on aliased commenter/commented post association but cannot get it, might give up til I can ask for advice or help

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+
+
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
