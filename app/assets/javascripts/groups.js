3a9480fbc81e12fbd63a9f662b544677fb2dad49
Peter-G-Stone <peter.stone.online@gmail.com>
2019-02-09 17:13:53 -0500

    now when you click "new post" on the group show page it renders a form right there using jquery

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,10 +1,59 @@
 // will get the group data, allow display of posts and comments 
 
+function appendPost(post, groupId, currentUserId){
+    const editUrl = "/posts/" + post.id + '/edit'
+    let postCard = '<div class="card blue-grey darken-4">' +
+        '<div class="card-content white-text">' +
+        '<span class="card-title"><a href="//localhost:3000/profiles/' +
+        post.user.id +
+        '" >' +
+        post.user.name +
+        '</a></span>' +
+        '<p><br><a href="//localhost:3000/groups/' +
+        groupId +
+        '/posts/' +
+        post.id +
+        '">' +
+        post.content +
+        '</a><br>Created: ' + 
+                    // previously did this strftime
+                    // <%= post.created_at.strftime("%A, %d %b %Y %l:%M %p") %> UTC
+        post.created_at +
+        '</p></div>' +
+        '<div class="card-action">'
+    if (currentUserId === post.user.id){
+        const editAndDelete = '<p><a href="//localhost:3000/groups/'+
+            groupId +
+            editUrl +
+            '">Edit Post</a></p>' +
+            '<button>Delete</button>'
+            // make this button actually do something pete!
+        postCard += editAndDelete
+    }
+    postCard += '</div></div>'
+    $('#group-show-post-list').append(postCard)
+}
+
+function renderNewPostForm(groupId) {
+    $('#new-post-link').on('click', function (event){
+        event.preventDefault()
+        console.log('you clikced me')
+        console.log(this)
+        $('#new-post-link').text('')
+        const postingUrl = '/groups/' + groupId + '/posts/new'
+        const newPostForm = '<form><input type="text" name="content" placeholder="New Post" method="post" action="' +
+            postingUrl +
+            '"><input type="submit"></form>'
+        $(newPostForm).insertAfter(this)
+    })
+}
+
+
+
 $(() => {
    
    
-    // the following is to render the posts on the page!
-
+    // the following is to render the group and posts data on the page!
     let groupId = $('.groupId').data("id")
     let currentUserId = $('.currentUserId').data("id")
     $.get("/groups/" + groupId + ".json", function(group) {
@@ -13,46 +62,15 @@ $(() => {
 
         const posts = group.posts
         posts.forEach( post => {
-            
-            // previously the post content linked to the post show page
-            // <%= link_to post.content, group_post_path(@group.id, post.id) %> 
-            // now I refactor to get rid of post show pages?
-
-            // previously did this strftime
-            // <%= post.created_at.strftime("%A, %d %b %Y %l:%M %p") %> UTC
-            const editUrl = "/posts/" + post.id + '/edit'
-            let postCard = '<div class="card blue-grey darken-4">' +
-                '<div class="card-content white-text">' +
-                '<span class="card-title"><a href="//localhost:3000/profiles/' +
-                post.user.id +
-                '" >' +
-                post.user.name +
-                '</a></span>' +
-                '<p><br><a href="//localhost:3000/groups/' +
-                groupId +
-                '/posts/' +
-                post.id +
-                '">' +
-                post.content +
-                '</a><br>Created: ' + 
-                post.created_at +
-                '</p></div>' +
-                '<div class="card-action">'
-            if (currentUserId === post.user.id){
-                const editAndDelete = '<p><a href="//localhost:3000/groups/'+
-                    groupId +
-                    editUrl +
-                    '">Edit Post</a></p>' +
-                    '<button>Delete</button>'
-                    // make this button actually do something pete!
-                    postCard += editAndDelete
-            }
-            postCard += '</div></div>'
-            $('#group-show-post-list').append(postCard)        
+            appendPost(post, groupId, currentUserId)                    
         })        
     })
 
-    // the following handles the form for new post:
+    ///////////////////
+
+    // the following renders the form when new post is clicked:
+    renderNewPostForm(groupId)
+
     
 })
 
@@ -60,7 +78,10 @@ $(() => {
 
 
 
+
+
 // this is popped in here cause I was curious about it and want to look at it later:
+
 // $(document).ready( () => {
 
 // })
