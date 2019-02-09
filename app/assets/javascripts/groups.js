c110f457b25c0a9f8854828e38a94593920dbc67
Peter-G-Stone <peter.stone.online@gmail.com>
2019-02-09 10:59:42 -0500

    continuing to work on groups.js to provide the group show data via json rather than via a rails/ruby object

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -3,21 +3,33 @@
 $(() => {
     let groupId = $('.groupId').data("id")
     $.get("/groups/" + groupId + ".json", function(group) {
-        $("#group-show-group-name").html('<h1>' + group.name + ' from js!</h1>')
-        $("#group-show-group-summary").html(group.summary + ' from js!')
-        $("#group-show-group-summary").html(group.summary + ' from js!')
+        $("#group-show-group-name").html('<h1>' + group.name + '</h1>')
+        $("#group-show-group-summary").html(group.summary)
 
         const posts = group.posts
         posts.forEach( post => {
             console.log(post)
             
-            // got user and comment info in json! yay!
+            // previously the post content linked to the post show page
+            // <%= link_to post.content, group_post_path(@group.id, post.id) %> 
+            // now I refactor to get rid of post show pages?
+
+            // previously did this strftime
+            // <%= post.created_at.strftime("%A, %d %b %Y %l:%M %p") %> UTC
             const postCard = '<div class="card blue-grey darken-4">' +
                 '<div class="card-content white-text">' +
-                    '<span class="card-title">this is content<%= link_to post.user.name, profile_path(post.user) %></span>' +
-                    '<p><%= link_to post.content, group_post_path(@group.id, post.id) %> <br>' +
-                    'Created: <%= post.created_at.strftime("%A, %d %b %Y %l:%M %p") %> UTC</p>' +
-                '</div>' +
+                '<span class="card-title"><a href="//localhost:3000/profiles/' +
+                post.user.id +
+                '" >' +
+                post.user.name +
+                '</a></span>' +
+                '<p><br><a href="/posts/' +
+                post.id +
+                '">' +
+                post.content +
+                '</a><br>Created: ' + 
+                post.created_at +
+                '</p></div>' +
                 '<div class="card-action">' +        
                     '<% if post.user == current_user %>' +
                         '<p><%= link_to "Edit Post", edit_group_post_path(@group.id, post.id) %>' +
