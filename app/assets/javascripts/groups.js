9677ef005ab5578ef9854898fb15c323be847d54
Peter-G-Stone <peter.stone.online@gmail.com>
2019-02-10 15:05:49 -0500

    reworking code into invidividual functions in groups.js so it is readable

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -15,7 +15,7 @@ class Post {
     }
 
     // prototype function will be to generate post card
-    postCard() {
+    prependPost() {
         let postCardHtml = '<div class="card blue-grey darken-4">' +
             '<div class="card-content white-text">' +
             '<span class="card-title"><a href="//localhost:3000/profiles/' +
@@ -48,7 +48,7 @@ class Post {
                 postCardHtml += editAndDelete
             }
         postCardHtml += '</div></div>'
-        return postCardHtml
+        $('#group-show-post-list').prepend(postCardHtml)
     }
 }
 
@@ -57,11 +57,10 @@ class Post {
 
 
 
-function prependPost(post, groupId, currentUserId){    
-    let postObj = new Post(post, groupId, currentUserId)
-    let postCard = postObj.postCard()   
-    $('#group-show-post-list').prepend(postCard)
-}
+// function prependPost(post, groupId, currentUserId){    
+//     let postObj = new Post(post, groupId, currentUserId)
+//     let postCard = postObj.postCard()   
+// }
 
 function newPostListener() {
     $('#new-post-link').on('click', function (event){
@@ -71,55 +70,54 @@ function newPostListener() {
     })
 }
 
+function newPostSubmitter(groupId, currentUserId) {
+    $('#new_post').submit(function (event) {
+        event.preventDefault()
+        const newPost = $('#new_post').serialize()
+        // const content = $('#post_content').val()
+        // console.log(content)
+        let posting = $.post('/groups/' + groupId + '/posts', newPost)
+        
+        posting.done( function(post){
+            postObj = new Post (post, groupId, currentUserId)
+            postObj.prependPost()
+            $('input[type="text"], textarea').val('')
+            $('#new-post-link').show()
+            $('form').hide()
+        })
+        return false // this is what you were missing for the longest time pete!
+    })
+}
+
+function renderGroupAndPosts(groupId, currentUserId){
+    $.get("/groups/" + groupId + ".json", function(group) { // get the group data
+        $("#group-show-group-name").html('<h1>' + group.name + '</h1>')
+        $("#group-show-group-summary").html(group.summary)
+        
+        const posts = group.posts
+        posts.forEach( post => { 
+            postObj = new Post (post, groupId, currentUserId) 
+            postObj.prependPost()                    
+        })        
+    })
+}
 
 
 $(document).on('turbolinks:load', function () { //had to change to turbolinks:load 
-                                                // listener because of turbolinks breaking 
+                                                // listener because of Rails' turbolinks breaking 
                                                 // jquery's ready - explained here: 
                                                 // https://stackoverflow.com/questions/18769109/rails-4-turbo-link-prevents-jquery-scripts-from-working/18770219#18770219
     
     
-    
-    
     if ($('.groupShowPage').data("id") === true){ 
-        // if this page is the group Show page, create post list and handle new post submissions:
-        
-        
-        
-        // the following is to render the group and posts data on the page!
-        
+        // if this page is the group Show page, render group data, post list and handle new post submissions:
         const groupId = $('.groupId').data("id") // this is id of the current group page being shown
-        const currentUserId = $('.currentUserId').data("id")  // id of the current logged on user        
-        $.get("/groups/" + groupId + ".json", function(group) { // get the group data
-            $("#group-show-group-name").html('<h1>' + group.name + '</h1>')
-            $("#group-show-group-summary").html(group.summary)
-            
-            const posts = group.posts
-            posts.forEach( post => { 
-                prependPost(post, groupId, currentUserId)                    
-            })        
-        })
+        const currentUserId = $('.currentUserId').data("id")  // id of the current logged on user     
         
-        
-        $('#new_post').hide() // hide the new post form (until it is summoned with 'new post' link)
+        renderGroupAndPosts(groupId, currentUserId)                
+        $('#new_post').hide() // hide the new post form (until it is summoned with 'new post' link via newPostListener())
         newPostListener()
-    
-        // this handles new post submission from that previously mentioned new_post form
-        $('#new_post').submit(function (event) {
-            event.preventDefault()
-            const newPost = $('#new_post').serialize()
-            // const content = $('#post_content').val()
-            // console.log(content)
-            let posting = $.post('/groups/' + groupId + '/posts', newPost)
-            
-            posting.done( function(post){
-                prependPost(post, groupId, currentUserId)
-                $('input[type="text"], textarea').val('')
-                $('#new-post-link').show()
-                $('form').hide()
-            })
-            return false // this is what you were missing for the longest time pete!
-        })
-    }
-    
+
+        newPostSubmitter(groupId, currentUserId) //  handles new post submission
+    }   
 })
